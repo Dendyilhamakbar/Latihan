1db7fbb94526c9b1ca331999df0c8c95b51814f4
Dendyilhamakbar <dendyilhamakbar1@gmail.com>
2017-11-06 09:59:32 +0800

    Latihan Soal 4 & 5

---
+++ Soal Nomor 4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	float integer;
+	integer = 0;
+	cout <<"Silahkan masukkan nilai/angka yang benar\n"<<endl;
+	cin >> integer;
+	
+	if
+	(integer > 0)
+	
+	cout << "Nilai yang anda masukkan adalah positif\n" <<endl;
+	
+	else ;
+	if
+	(integer < 0)
+	cout << "Nilai yang anda masukkan adalah negatif\n" <<endl;
+	
+	else;
+	if
+	(integer == 0)
+	cout << "Nilai yang anda masukkan adalah NOL \n" <<endl;
+	
+	else;
+	
+	return 0;
+}
